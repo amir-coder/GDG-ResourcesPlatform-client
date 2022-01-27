9699703ecbcdcdcea82fab5c1480825844435656
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-01-27 05:20:58 +0100

    homepage last

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import React from 'react';
-import './App.css'
-import Sidebar from './Components/sidebar/Sidebar';
-import Profile from './pages/Profile/Profile';
-import Home from './pages/Home';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import Login from './pages/Login/login';
-import EditProfile from "./pages/EditProfile/EditProfile"
-import Form from './pages/Register/Form';
-import RessourcePage from './pages/Ressorces/List';
-
-
+import React from "react";
+import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter >
-        <Sidebar />
-        <Routes>
-           <Route path="/" element={<Home />}></Route>
-           <Route path="/login" element={ <Login />}></Route>
-           <Route path="/register" element={<Form />} />
-           <Route path="/profile" element={<Profile />} />
-           <Route path="/editProfile" element={<EditProfile />} />
-           <Route path="/ressorces/:id" element={<RessourcePage />} />
-        </Routes>
-      </BrowserRouter>
-      </div>
-   
+    <div className="App">
+      <Homepage />
+    </div>
   );
 }
 export default App;
